f77e79ad3e1b0fda674b9da25eb1ab0696956dd2
Tong Jia <tong.tony.jia@gmail.com>
2016-10-12 22:03:10 -0400

    Add LinkedList library.

---
+++ lib/LinkedList.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def AddNode(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+
+        if self.tail is not None:
+            self.tail.next = new_node
+
+        self.tail = new_node
+
+    def RemoveNode(self, index):
+        prev = None
+        node = self.head
+        i = 0
+
+        while (node is not None) and(i < index):
+            prev = node
+        node = node.next
+        i += 1
+
+        if prev is None:
+            self.head = node.next
+        else:
+            prev.next = node.next
+
+    def PrintList(self):
+        node = self.head
+        while node is not None:
+            print node.data
+            node = node.next
+        print ""
